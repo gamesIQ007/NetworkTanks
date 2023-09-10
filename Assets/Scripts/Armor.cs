8ec9e98cc22fb78f6c05cdc9337da6886b40dbd7
gamesIQ007 <bdagames@ya.ru>
2023-09-10 14:07:49 +0300

    46.2. Броня

---
+++ Assets/Scripts/Armor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace NetworkTanks
+{
+    /// <summary>
+    /// Тип брони
+    /// </summary>
+    public enum ArmorType
+    {
+        /// <summary>
+        /// Транспорт
+        /// </summary>
+        Vehicle,
+        /// <summary>
+        /// Модули
+        /// </summary>
+        Module
+    }
+
+    /// <summary>
+    /// Броня
+    /// </summary>
+    public class Armor : MonoBehaviour
+    {
+        /// <summary>
+        /// Родитель
+        /// </summary>
+        [SerializeField] private Transform parent;
+
+        /// <summary>
+        /// Тип брони
+        /// </summary>
+        [SerializeField] private ArmorType type;
+        public ArmorType Type => type;
+
+        /// <summary>
+        /// Защищаемый дистрактибл
+        /// </summary>
+        [SerializeField] private Destructible destructible;
+        public Destructible Destructible => destructible;
+
+        /// <summary>
+        /// Толщина
+        /// </summary>
+        [SerializeField] private int thickness;
+        public int Thickness => thickness;
+
+
+        private void Awake()
+        {
+            transform.SetParent(parent);
+        }
+    }
+}
